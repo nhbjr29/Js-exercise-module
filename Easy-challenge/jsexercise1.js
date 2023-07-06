32c46088bde4bac44c04ab5435c9fa38ccf6699b
I'm Harold <nhb_jr@yahoo.com>
2023-07-06 10:00:17 +0800

    Changes to be committed: 	new file:   Easy-challenge/exercise1.html 	new file:   Easy-challenge/jsexercise1.js 	deleted:    jsexercise1.js     Organize folder

---
+++ Easy-challenge/jsexercise1.js
@@ -0,0 +1,11 @@
+//Exercise 1: Hello, World!
+let greet = "Hello, World!";
+console.log(greet);
+
+//Exercise 2: Variables and Data Types
+let firstName = "Erick";
+let age = 33;
+let isPlayer = true;
+let numbers = [1, 2, 3, 4, 5];
+let person = { name: "Donna", age: 30 };
+console.log(firstName, age, isPlayer, numbers, person);
